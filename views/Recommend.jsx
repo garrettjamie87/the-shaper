ec56e1977f2cdcc3082516941c44e85d97deec3b
Sergio Cabrera Capineti <sergiocabreracapineti@Sergios-MBP.home>
2020-11-26 11:11:59 +0100

    Update files

--- views/Recommend.jsx
+++ views/Recommend.jsx
@@ -2,24 +2,22 @@ const React = require("react");
 const Layout = require("./Layout");
 
 function Recommend(props) {
- return(
+  return (
     <Layout>
-    
       <h1>These are what we recommend</h1>
       {props.boards.map((el, i) => {
-    return (
-      <ol key={i}>
-<a href = {`/${el._id}/detail`}>{el.name}</a>
-</ol>
-    )
-  })}
-     
-      <button>
-      <a href="/Dashboard">View All</a>
-      </button>
+        return (
+          <ol key={i}>
+            <a href={`/detail/${el._id}`}>{el.name}</a>
+          </ol>
+        );
+      })}
 
+      <button>
+        <a href="/dashboard">View All</a>
+      </button>
     </Layout>
-  
- )}
+  );
+}
 
 module.exports = Recommend;
