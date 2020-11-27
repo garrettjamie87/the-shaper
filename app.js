fb8b713f44913793e9080e873321767a8e46b612
Jamie Garrett <garrettjamie87@googlemail.com>
2020-11-27 11:07:56 +0100

    image error

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
 // SESSIONS MIDDLEWARE
 app.use(
